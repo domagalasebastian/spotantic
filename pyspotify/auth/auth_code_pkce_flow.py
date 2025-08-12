b5385bcc442bd1fe423356353d8868bd00e2a50e
sdomagala <domagalasebastian8991@gmail.com>
2025-08-12 23:04:40 +0200

    Add logging

--- pyspotify/auth/auth_code_pkce_flow.py
+++ pyspotify/auth/auth_code_pkce_flow.py
@@ -14,6 +14,8 @@ REFRESH_AUTH_CODE_PKCE_FLOW_GRANT_TYPE = "refresh_token"
 
 class AuthCodePKCEFlowManager(RefreshableAuthManager):
     async def authorize(self) -> AccessTokenInfo:
+        self._logger.info("Starting Authorization Code PKCE Flow")
+        self._logger.debug(f"Current auth settings: {self.auth_settings}")
         assert self.auth_settings.client_id is not None, "Client ID must be set"
         assert self.auth_settings.redirect_uri is not None, "Redirect URI must be set"
         assert self.auth_settings.scope is not None, "Scope must be set"
@@ -32,6 +34,8 @@ class AuthCodePKCEFlowManager(RefreshableAuthManager):
             "code_challenge_method": AUTH_CODE_PKCE_FLOW_CODE_CHALLENGE_METHOD,
             "code_challenge": code_challenge,
         }
+        self._logger.info(f"Receiving auth code. Website will be hosted at {redirect_uri}")
+        self._logger.debug(f"Hosting Web Server with URL params: {params}")
         code = await get_code(redirect_uri=redirect_uri, params=params)
 
         data = {
@@ -41,24 +45,32 @@ class AuthCodePKCEFlowManager(RefreshableAuthManager):
             "client_id": self.auth_settings.client_id,
             "code_verifier": code_verifier,
         }
+        self._logger.info("Receiving access token from the server.")
+        self._logger.debug(f"Requesting access token with data: {data}")
         token_info = await get_access_token(data=data)
 
         if self.auth_settings.store_access_token:
+            self._logger.info(f"Saving access token to file: {self.auth_settings.access_token_file_path}")
             token_info.store_token(file_path=self.auth_settings.access_token_file_path)
 
         return token_info
 
     async def refresh(self, refresh_token: str) -> AccessTokenInfo:
         assert self.auth_settings.client_id is not None, "Client ID must be set"
+        self._logger.info("Refreshing access token with Authorization Code PKCE Flow")
+        self._logger.debug(f"Current auth settings: {self.auth_settings}")
 
         data = {
             "grant_type": REFRESH_AUTH_CODE_PKCE_FLOW_GRANT_TYPE,
             "refresh_token": refresh_token,
             "client_id": self.auth_settings.client_id,
         }
+        self._logger.info("Receiving access token from the server.")
+        self._logger.debug(f"Requesting access token with data: {data}")
         token_info = await get_access_token(data=data)
 
         if self.auth_settings.store_access_token:
+            self._logger.info(f"Saving access token to file: {self.auth_settings.access_token_file_path}")
             token_info.store_token(file_path=self.auth_settings.access_token_file_path)
 
         return token_info
--- pyspotify/auth/client_credentials_flow.py
+++ pyspotify/auth/client_credentials_flow.py
@@ -9,6 +9,8 @@ CLIENT_CREDENTIALS_FLOW_GRANT_TYPE = "client_credentials"
 
 class ClientCredentialsFlowManager(AuthManagerBase):
     async def authorize(self) -> AccessTokenInfo:
+        self._logger.info("Starting Client Credentials Flow")
+        self._logger.debug(f"Current auth settings: {self.auth_settings}")
         assert self.auth_settings.client_id is not None, "Client ID must be set"
         assert self.auth_settings.client_secret is not None, "Client Secret must be set"
 
@@ -17,9 +19,12 @@ class ClientCredentialsFlowManager(AuthManagerBase):
             "grant_type": CLIENT_CREDENTIALS_FLOW_GRANT_TYPE,
         }
 
+        self._logger.info("Receiving access token from the server.")
+        self._logger.debug(f"Requesting access token with data: {data}")
         token_info = await get_access_token(data=data, auth=auth_header)
 
         if self.auth_settings.store_access_token:
+            self._logger.info(f"Saving access token to file: {self.auth_settings.access_token_file_path}")
             token_info.store_token(file_path=self.auth_settings.access_token_file_path)
 
         return token_info
