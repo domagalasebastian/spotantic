609fda11e29d71061987c409ca2aea3c858db95a
Sebastian Domagała <64256966+domagalasebastian@users.noreply.github.com>
2025-08-16 14:50:53 +0200

    Add example of client creation

---
+++ examples/create_client.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from pyspotify.auth import AuthCodeFlowManager
+from pyspotify.auth import AuthSettings
+from pyspotify.client import PySpotifyClient
+
+
+async def create_pyspotify_client() -> PySpotifyClient:
+    auth_settings = AuthSettings()
+    auth_manager = AuthCodeFlowManager(auth_settings=auth_settings)
+
+    access_token_info = await auth_manager.authorize()
+    client = PySpotifyClient(auth_manager=auth_manager, access_token_info=access_token_info)
+    await client.setup_client_session()
+
+    return client
+
+
+if __name__ == "__main__":
+    asyncio.run(create_pyspotify_client())
