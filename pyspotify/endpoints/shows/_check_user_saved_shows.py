1b7dc47cd7b81946af81e9f8c9f03aad6aa20e1d
Sebastian Domagała <64256966+domagalasebastian@users.noreply.github.com>
2025-12-13 19:32:46 +0100

    WIP

--- pyspotify/endpoints/shows/_check_user_saved_shows.py
+++ pyspotify/endpoints/shows/_check_user_saved_shows.py
@@ -6,20 +6,28 @@ from pyspotify.custom_types import APIResponse
 from pyspotify.custom_types import SpotifyItemID
 from pyspotify.models import APICallModel
 from pyspotify.models.shows.requests import CheckUserSavedShowsRequest
-from pyspotify.models.shows.requests import CheckUserSavedShowsRequestParams
 
 
 async def check_user_saved_shows(
     client: PySpotifyClient, *, show_ids: Sequence[SpotifyItemID]
 ) -> APICallModel[CheckUserSavedShowsRequest, APIResponse, Dict[SpotifyItemID, bool]]:
-    request = CheckUserSavedShowsRequest(
-        endpoint="me/shows/contains",
-        params=CheckUserSavedShowsRequestParams(
-            show_ids=show_ids,
-        ),
+    """Check if given shows are saved in the user's library.
+
+    Check if one or more shows is already saved in the current Spotify user's library.
+
+    Args:
+        client: PySpotifyClient instance.
+        show_ids: A list of the Spotify IDs for the shows.
+
+    Returns:
+        An object containing the request used to obtain the response, the retrieved data and
+        parsed data as model.
+    """
+    request = CheckUserSavedShowsRequest.build(
+        show_ids=show_ids,
     )
     response = await client.request(request)
     assert response is not None
-    data = dict(zip(show_ids, response))
+    data = dict(zip(show_ids, response, strict=True))
 
     return APICallModel(request=request, response=response, data=data)
--- pyspotify/models/shows/requests/_check_user_saved_shows.py
+++ pyspotify/models/shows/requests/_check_user_saved_shows.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from http import HTTPMethod
 from typing import Annotated
+from typing import Optional
 from typing import Sequence
 
 from pydantic import BaseModel
@@ -12,6 +15,8 @@ from pyspotify.models import RequestModel
 
 
 class CheckUserSavedShowsRequestParams(BaseModel):
+    """Params model for Check User Saved Shows request."""
+
     model_config = ConfigDict(serialize_by_alias=True)
 
     show_ids: Annotated[
@@ -19,7 +24,33 @@ class CheckUserSavedShowsRequestParams(BaseModel):
         Field(max_length=50, serialization_alias="ids"),
         PlainSerializer(lambda seq: ",".join(seq), return_type=str),
     ]
+    """A list of the Spotify IDs for the shows."""
 
 
 class CheckUserSavedShowsRequest(RequestModel[CheckUserSavedShowsRequestParams, None]):
+    """Request model for Check User Saved Shows endpoint."""
+
     method_type: HTTPMethod = HTTPMethod.GET
+    """HTTP method for the request."""
+
+    endpoint: Optional[str] = "me/shows/contains"
+    """Endpoint associated with the request."""
+
+    @classmethod
+    def build(cls, *, show_ids: Sequence[SpotifyItemID]) -> CheckUserSavedShowsRequest:
+        """Builds a request model based on given parameters.
+
+        The function automatically determines the endpoint if it is not static.
+        Also, it automatically assigns parameters to request query or body.
+
+        Args:
+            show_ids: A list of the Spotify IDs for the shows.
+
+        Returns:
+            Validated Request object.
+        """
+        params = CheckUserSavedShowsRequestParams(
+            show_ids=show_ids,
+        )
+
+        return cls(params=params)
