30a33cc4e5c86c73290ff0c7e7f24638ef5fab76
Sebastian Domagała <64256966+domagalasebastian@users.noreply.github.com>
2026-02-02 20:48:34 +0100

    Refactor categories endpoint and update API references

--- pyspotify/endpoints/categories/_get_several_browse_categories.py
+++ pyspotify/endpoints/categories/_get_several_browse_categories.py
@@ -5,7 +5,6 @@ from pyspotify.custom_types import APIResponse
 from pyspotify.custom_types import SpotifyLocaleID
 from pyspotify.models import APICallModel
 from pyspotify.models.categories.requests import GetSeveralBrowseCategoriesRequest
-from pyspotify.models.categories.requests import GetSeveralBrowseCategoriesRequestParams
 from pyspotify.models.spotify import CategoryModel
 from pyspotify.models.spotify import PagedResultModel
 
@@ -13,13 +12,26 @@ from pyspotify.models.spotify import PagedResultModel
 async def get_several_browse_categories(
     client: PySpotifyClient, *, limit: int = 20, offset: int = 0, locale: Optional[SpotifyLocaleID] = None
 ) -> APICallModel[GetSeveralBrowseCategoriesRequest, APIResponse, PagedResultModel[CategoryModel]]:
-    request = GetSeveralBrowseCategoriesRequest(
-        endpoint="browse/categories",
-        params=GetSeveralBrowseCategoriesRequestParams(
-            limit=limit,
-            offset=offset,
-            locale=locale,
-        ),
+    """Get Spotify catalog information for several browse categories.
+
+    Get a list of categories used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab).
+
+    Args:
+        client: PySpotifyClient instance.
+        limit: The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+        offset: The index of the first item to return. Default: 0 (the first item).
+          Use with limit to get the next set of items.
+        locale: The desired language, consisting of an ISO 639-1 language code and an ISO 3166-1 alpha-2 country code,
+         joined by an underscore.
+
+    Returns:
+        An object containing the request used to obtain the response, the retrieved data and
+        parsed data as model.
+    """
+    request = GetSeveralBrowseCategoriesRequest.build(
+        limit=limit,
+        offset=offset,
+        locale=locale,
     )
     response = await client.request(request)
     assert response is not None
--- pyspotify/endpoints/categories/_get_single_browse_category.py
+++ pyspotify/endpoints/categories/_get_single_browse_category.py
@@ -5,19 +5,29 @@ from pyspotify.custom_types import APIResponse
 from pyspotify.custom_types import SpotifyLocaleID
 from pyspotify.models import APICallModel
 from pyspotify.models.categories.requests import GetSingleBrowseCategoryRequest
-from pyspotify.models.categories.requests import GetSingleBrowseCategoryRequestParams
 from pyspotify.models.spotify import CategoryModel
 
 
 async def get_single_browse_category(
     client: PySpotifyClient, *, category_id: str, locale: Optional[SpotifyLocaleID] = None
 ) -> APICallModel[GetSingleBrowseCategoryRequest, APIResponse, CategoryModel]:
-    request = GetSingleBrowseCategoryRequest(
-        endpoint=f"browse/categories/{category_id}",
-        params=GetSingleBrowseCategoryRequestParams(
-            category_id=category_id,
-            locale=locale,
-        ),
+    """Get Spotify catalog information for a single browse category.
+
+    Get a single category used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab).
+
+    Args:
+        client: PySpotifyClient instance.
+        category_id: The Spotify ID for the browse category.
+        locale: The desired language, consisting of an ISO 639-1 language code and an ISO 3166-1 alpha-2 country code,
+         joined by an underscore.
+
+    Returns:
+        An object containing the request used to obtain the response, the retrieved data and
+        parsed data as model.
+    """
+    request = GetSingleBrowseCategoryRequest.build(
+        category_id=category_id,
+        locale=locale,
     )
     response = await client.request(request)
     assert response is not None
