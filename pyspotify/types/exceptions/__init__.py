5866da346ecd2bf413b102169f5118906fe7bf37
Sebastian Domagała <64256966+domagalasebastian@users.noreply.github.com>
2026-02-16 19:53:52 +0100

    Add scope validation and insufficient scope error handling in PySpotifyClient

--- pyspotify/types/exceptions/__init__.py
+++ pyspotify/types/exceptions/__init__.py
@@ -1,10 +1,12 @@
 from ._custom_exceptions import PySpotifyException
+from ._custom_exceptions import PySpotifyInsufficientScopeError
 from ._custom_exceptions import PySpotifyResponseError
 from ._custom_exceptions import PySpotifyTooManyRequests
 from ._custom_exceptions import PySpotifyUnauthorizedError
 
 __all__ = [
     "PySpotifyException",
+    "PySpotifyInsufficientScopeError",
     "PySpotifyResponseError",
     "PySpotifyTooManyRequests",
     "PySpotifyUnauthorizedError",
