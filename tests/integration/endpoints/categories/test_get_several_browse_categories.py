12af214b2dd8cde9445ca61c0aad7e38facb41c9
Sebastian Domagała <64256966+domagalasebastian@users.noreply.github.com>
2026-03-18 19:53:55 +0100

    Add integration tests for categories endpoint

---
+++ tests/integration/endpoints/categories/test_get_several_browse_categories.py
@@ -0,0 +1,18 @@
+import pytest
+
+from spotantic.endpoints.categories import get_several_browse_categories
+from spotantic.models.categories.requests import GetSeveralBrowseCategoriesRequest
+from spotantic.models.spotify import CategoryModel
+from spotantic.models.spotify import PagedResultModel
+
+
+@pytest.mark.asyncio
+@pytest.mark.readonly
+async def test_get_several_browse_categories(client):
+    result = await get_several_browse_categories(client, limit=50)
+
+    assert isinstance(result.request, GetSeveralBrowseCategoriesRequest)
+    assert isinstance(result.response, dict)
+    assert isinstance(result.data, PagedResultModel)
+    if result.data.items:
+        assert isinstance(result.data.items[0], CategoryModel)
